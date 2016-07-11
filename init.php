b7d95ebef924592c938b9ff4915586fff4b37d36
Kyle Meredith <kylemeredith@mail.com>
2016-07-11 14:24:14 -0400

    Update init.php

--- init.php
+++ init.php
@@ -6,6 +6,6 @@ $_SESSION['user_id'] = 1;
 
 $db = new PDO('mysql:dbname=todo;host=localhost', 'root', 'root');
 
-if(!isset($_SESSION)['user_id'])){
+if(!isset($_SESSION)['user_id']){
     die('You are not signed in.');
 }
