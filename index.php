26608a2643240956b91ac0f866a54a824b925ef6
Kyle Meredith <kylemeredith@mail.com>
2016-07-11 13:46:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,49 @@
 <?php
-
-require_once 'app/init.php';
-
-$itemsQuery = $db->prepare("
+	require_once ("app/init.php");
+	$itemsQuery = $db->prepare("
     SELECT id, name, done
     FROM items
     WHERE user = :user
 ");
+	$itemsQuery->execute([    'user' => $_SESSION['user_id']]);
+	$items = $itemsQuery->rowCount() ? $itemsQuery :
+	[];
+	foreach ($items as item) {
+		print_r ($item);
+	}
 
-$itemsQuery->execute([
-    'user' => $_SESSION['user_id']
-]);
-
-$items = $itemsQuery->rowCount() ? $itemsQuery : [];
-
-foreach($items as item) {
-    print_r($item);
-}
-
-?>
-
-
+	?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>To Do</title>
-    
         <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
         <link rel="stylesheet" href="main.css">
-        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <div class="list">
             <h1 class="header">To Do.</h1>   
-
-            <?php if(!empty($items)): ?>
-
+            <?php  if(!empty($items)): ?>
             <ul class="items">
-                    <?php foreach($items as $item): ?>
+                    <?php  foreach($items as $item): ?>
                     <li>
-                        <span class="item"<?php echo $item['done'] ? ' done' : '' ?><?php echo $item['name']: ?></span>
-                        <?php if(!$item['done']): ?>
-                            <a href="mark.php?as=done&item=<?php echo $item['id']; ?>" class="done-button"> Mark as done</a>
-                        <?php endif; ?>
+                        <span class="item"<?php  echo $item['done'] ? ' done' :''  ?><?php  echo $item['name']: ?></span>
+                        <?php  if(!$item['done']): ?>
+                            <a href="mark.php?as=done&item=<?php  echo $item['id']; ?>" class="done-button"> Mark as done</a>
+                        <?php  endif; ?>
                     </li>
-                    <?php> endforeach; ?>
+                    <?php php> endforeach; ?>
                     </ul>
-
-                    <?php else:?>
+                    <?php  else : ?>
                         <p>You haven't added any items yet.</p>
-                    <?php endif;?>
-
+                    <?php  endif; ?>
                     <form class="item-add" action="add.php" method="post">
                         <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
                         <input type="submit" value="Add" class="submit">
                     </form>
-
         </div>
     </body>
 </html>
