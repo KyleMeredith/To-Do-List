3882d450b745e9e48da7c8623b26913522f4896c
Kyle Meredith <kylemeredith@mail.com>
2016-07-11 14:12:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -30,12 +30,12 @@
             <ul class="items">
                     <?php  foreach($items as $item): ?>
                     <li>
-                        <span class="item"<?php  echo $item['done'] ? ' done' :''  ?><?php  echo $item['name']: ?></span>
+                        <span class="item"<?php  echo $item['done'] ? ' done' :''  ?><?php  echo $item['name']; ?></span>
                         <?php  if(!$item['done']): ?>
                             <a href="mark.php?as=done&item=<?php  echo $item['id']; ?>" class="done-button"> Mark as done</a>
                         <?php  endif; ?>
                     </li>
-                    <?php php> endforeach; ?>
+                    <?php endforeach; ?>
                     </ul>
                     <?php  else : ?>
                         <p>You haven't added any items yet.</p>
